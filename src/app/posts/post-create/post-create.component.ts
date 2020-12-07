1b34d1842926baa3c1fb25919cdbf3b3555af2b4
dlynnea <danimatton@gmail.com>
2020-12-06 21:57:25 -0700

    some initial frontend set up

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent implements OnInit {
+  enteredContent = '';
+  enteredTitle = '';
+
+  constructor(public postsService: PostsService) { }
+
+  ngOnInit(): void {
+  }
+
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.postsService.addPost(form.value.title, form.value.content)
+    form.resetForm();
+  }
+
+}
